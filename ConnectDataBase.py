57f2cc5bf84e0a259910c5527f52bda2872f8722
MaximShubin7 <bfhf2222@gmail.com>
2025-05-07 01:59:58 +0500

    third

--- ConnectDataBase.py
+++ ConnectDataBase.py
@@ -1,11 +1,19 @@
+import os
 import psycopg2
+from urllib.parse import urlparse
 
 
 def get_db_connection():
+    database_url = os.getenv('DATABASE_URL')
+    if not database_url:
+        raise ValueError("DATABASE_URL не задан в переменных окружения")
+    result = urlparse(database_url)
+
     return psycopg2.connect(
-        dbname="DataBase",
-        user="postgres",
-        password="qwerty123",
-        host="localhost",
-        port=3000
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port or 5432,
+        sslmode="require"
     )
