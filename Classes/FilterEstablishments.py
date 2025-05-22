3cb64e51b4f4cb5d6dcc402e6c5dfb3bbd65d75e
MaximShubin7 <bfhf2222@gmail.com>
2025-05-22 15:42:44 +0500

    fix filter

--- Classes/FilterEstablishments.py
+++ Classes/FilterEstablishments.py
@@ -4,5 +4,5 @@ from typing import Optional
 class FilterEstablishments(BaseModel):
     name: Optional[str] = None
     rating: Optional[float] = Field(None, ge=1, le=5)
-    latitude: float = Field(..., ge=-90, le=90)
-    longitude: float = Field(..., ge=-180, le=180)
+    latitude: Optional[float] = Field(None, ge=-90, le=90)
+    longitude: Optional[float] = Field(None, ge=-180, le=180)
